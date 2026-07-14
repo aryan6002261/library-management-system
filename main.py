e5688672f1211e7611a20237b01570e430347fb9
Aryan Sharma <aryan6002261@gmail.com>
2026-07-14 21:48:02 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from library import Library
+from book import Book
+from utils import menu
+
+library = Library()
+
+while True:
+    menu()
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        book_id = input("Book ID: ")
+        title = input("Title: ")
+        author = input("Author: ")
+        library.add_book(Book(book_id, title, author))
+
+    elif choice == "2":
+        library.show_books()
+
+    elif choice == "3":
+        book_id = input("Book ID: ")
+        library.issue_book(book_id)
+
+    elif choice == "4":
+        book_id = input("Book ID: ")
+        library.return_book(book_id)
+
+    elif choice == "5":
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice.")
